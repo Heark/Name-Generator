51934223cb6f667852fa3287b8e5b51d40e1aafc
Heark <adtune6509@student.waynecc.edu>
2016-09-12 14:45:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function randOutput() {
-
+ PA = partA[Math.floor(Math.random() * partA.length)];
+ PB = partB[Math.floor(Math.random() * partA.length)];
+ 
+ return PA + PB;
 }
+
+$("#generate").click(function(){
+  $("#output").html("<font color = red>"+randOutput()+"</font>");
+}) 
